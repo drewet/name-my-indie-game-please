1cb6c93c03c16c7555a41f8667681d65647f5e93
Nathaniel Theis <nttheis@gmail.com>
2014-10-09 22:21:10 -0700

    Use handles instead of just IDs

--- shared/src/entity/mod.rs
+++ shared/src/entity/mod.rs
@@ -6,13 +6,21 @@ pub struct EntityID(u32);
 
 #[deriving(Show)]
 /// Type parameter used only for correctness.
-pub struct ComponentID<PayloadTy>(u32);
-
+pub struct ComponentHandle<Payload> {
+    id: u16,
+    serial: u16
+}
+impl<Payload> PartialEq for ComponentHandle<Payload> {
+    fn eq(&self, other: &ComponentHandle<Payload>) -> bool {
+        self.id == other.id && self.serial == other.serial
+    }
+}
 pub struct Component<Payload> {
-    id: ComponentID<Payload>,
+    handle: ComponentHandle<Payload>,
     entity: EntityID,
     payload: Payload
 }
+/*
 impl<Payload> PartialEq for ComponentID<Payload> {
     fn eq(&self, other: &ComponentID<Payload>) -> bool {
         let (&ComponentID(thisid), &ComponentID(otherid)) = (self, other);
@@ -32,80 +40,68 @@ impl<Payload> PartialOrd for ComponentID<Payload> {
     }
 }
 impl<Payload> Eq for ComponentID<Payload> {}
-
+*/
 
 impl<Payload> PartialEq for Component<Payload> {
     fn eq(&self, other: &Component<Payload>) -> bool {
-        self.id == other.id
+        self.handle == other.handle
     }
 }   
-impl<Payload> Ord for Component<Payload> {
-    fn cmp(&self, other: &Component<Payload>) -> Ordering {
-        self.id.cmp(&other.id)
-    }
-}   
-impl<Payload> PartialOrd for Component<Payload> {
-    fn partial_cmp(&self, other: &Component<Payload>) -> Option<Ordering> {
-        self.id.partial_cmp(&other.id)
-    }
-}  
 impl<Payload> Eq for Component<Payload> {}
 
 impl<Payload> Component<Payload>  {
-    /// Returns the ID of this component.
-    fn get_id(&self) -> ComponentID<Payload> { self.id }
-    
+    fn get_handle(&self) -> ComponentHandle<Payload> { self.handle }
     /// Returns the ID of the entity this component is attached to.
     fn get_entity_id(&self) -> EntityID { self.entity }
 }
 
 pub struct ComponentStore<Payload> {
     components: Vec<Component<Payload>>,
-    next_id: ComponentID<Payload>
+    free_ids: Bitv
 }
 
 impl<Payload> ComponentStore<Payload> {
     pub fn new() -> ComponentStore<Payload> {
-        ComponentStore { components: Vec::new(), next_id: ComponentID(0) }
+        ComponentStore { components: Vec::with_capacity(512), free_ids: Bitv::with_capacity(512, true) }
     }
 
-    /// Entity IDs are used in a sort of backwards garbage collection scheme;
-    /// specifically, various component stores may occasionally run through
-    /// all the components contained therein, checking the entity ID of each one
-    /// against the EntityStore for liveness.
-    /// This should be a fairly infrequent operation.
-    // TODO: we can probably save reallocating here if we're clever
-    // is it worth it?
-    pub fn gc(self, is_ent_alive: |EntityID| -> bool) -> ComponentStore<Payload> {
-        ComponentStore {
-            components: self.components.into_iter()
-                .filter(|comp| is_ent_alive(comp.get_entity_id())).collect(),
-            next_id: self.next_id
-        }
-    }
-    
     pub fn iter(&self) -> std::slice::Items<Component<Payload>> { self.components.iter() }
 
-    fn bump_id(&mut self) {
-        let ComponentID(id) = self.next_id;
-        // beware of overflow. if we actually fail here,
-        // we'll cross that bridge when we come to it.
-        self.next_id = ComponentID(id.checked_add(&1).unwrap());
-    }
-
-    pub fn add_component(&mut self, entity: EntityID, payload: Payload) -> ComponentID<Payload> {
-        let id = self.next_id;
-        self.components.push(Component { id: id, entity: entity, payload: payload });
-        self.bump_id();
-        id
-    }
-
-    pub fn find(&self, id: &ComponentID<Payload>) -> Option<&Component<Payload>> {
-        use std::slice::{Found, NotFound};
-        let result = self.components[].binary_search(|probe| probe.get_id().cmp(id));
+    pub fn add_component(&mut self, entity: EntityID, payload: Payload) -> ComponentHandle<Payload> {
+        let result = self.components.iter().zip(self.free_ids.iter()).position(|(_, is_free)| is_free);
         match result {
-            Found(pos) => Some(&self.components[pos]),
-            NotFound(_) => None
+            Some(pos) => {
+                let old_serial = self.components[pos].handle.serial;
+                let handle = ComponentHandle { id: pos as u16, serial: old_serial.checked_add(&1).unwrap() };
+                *self.components.get_mut(pos) = Component {
+                    handle: handle,
+                    entity: entity,
+                    payload: payload
+                };
+                self.free_ids.set(pos, false);
+                handle
+            }
+            None => {
+                let handle = ComponentHandle {
+                    id: self.components.len() as u16,
+                    serial: 0
+                };
+                self.free_ids.push(false);
+                self.components.push(Component {
+                    handle: handle,
+                    entity: entity,
+                    payload: payload
+                });
+                handle
+            }
+        }
+    }
+
+    pub fn find(&self, handle: ComponentHandle<Payload>) -> Option<&Component<Payload>> {
+        if self.components[handle.id as uint].handle.serial == handle.serial {
+            Some(&self.components[handle.id as uint])
+        } else {
+            None
         }
      }
 }
@@ -131,7 +127,7 @@ impl EntityStore {
 #[cfg(test)]
 mod test {
     use test::Bencher;
-    use super::{ComponentID, EntityID, Component, ComponentStore, EntityStore};
+    use super::{ComponentHandle, EntityID, Component, ComponentStore, EntityStore};
     struct TestStringComponent {
         name: String
     }
@@ -143,24 +139,20 @@ mod test {
         let mut estore = EntityStore::new();
         let ent = estore.create_entity();
         let comp = cstore.add_component(ent, TestStringComponent { name: "Hello, world!".to_string()});
-        cstore = cstore.gc(|ref id| estore.is_alive(id));
-        assert_eq!(cstore.find(&comp).unwrap().payload.name.as_slice(), "Hello, world!");
+        assert_eq!(cstore.find(comp).unwrap().payload.name.as_slice(), "Hello, world!");
     }
 
     #[bench]
     fn bench_entcreation_singlecomponent(b: &mut Bencher) {
         let mut cstore = ComponentStore::new();
         b.iter(|| cstore.add_component(EntityID(0), TrivialComponent));
-        ::test::black_box(cstore);
     }
 
     #[bench]
     fn bench_componentid_lookup_2048(b: &mut Bencher) {
         let mut cstore = ComponentStore::new();
         let ids = Vec::from_fn(2048, |_| cstore.add_component(EntityID(0), TrivialComponent));
-        for id in ids.iter() {
-            b.iter(|| cstore.find(id));
-        }
+        b.iter(|| cstore.find(ids[ids.len()/2]));
     }
 
     #[bench]
