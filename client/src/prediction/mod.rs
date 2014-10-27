6422095d8db5fe2ba557c2ccf87857c39c072748
Nathaniel Theis <nttheis@gmail.com>
2014-10-26 21:41:48 -0700

    split off game

--- client/src/prediction/mod.rs
+++ client/src/prediction/mod.rs
@@ -3,11 +3,9 @@ use std::collections::Deque;
 use shared::{
     ComponentStore,
     EntityComponent,
-    EntityHandle,
 
     playercmd
 };
-use shared::network::UpdatePacket;
 use shared::network::channel::{overflow_aware_compare, SequenceNr};
 use shared::playercmd::{ControllableComponent, PlayerCommand};
 use cgmath::ApproxEq;
